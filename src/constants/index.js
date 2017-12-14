ee79e0e69827c5ca5fb019fe34f46cbd3f233f87
Leslie Zacharkow <lesliezacharkow@gmail.com>
2017-12-14 09:38:39 -0800

    Move firebase config to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,14 @@
 /* eslint-disable import/prefer-default-export */
 
+// Config
+export const FIREBASE_CONFIG = {
+	apiKey: 'AIzaSyB5171RmMdJzO131E_Wypm1o1uVsvWBfvw',
+	authDomain: 'leslie-and-snapdocs.firebaseapp.com',
+	databaseURL: 'https://leslie-and-snapdocs.firebaseio.com',
+	projectId: 'leslie-and-snapdocs',
+	storageBucket: '',
+	messagingSenderId: '404074347699',
+};
+
 // Actions
 export const SET_SAMPLE_NAME = 'SET_SAMPLE_NAME';
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,15 +1,8 @@
 import * as firebase from 'firebase';
+import { FIREBASE_CONFIG } from '../constants';
 
 // Initialize
-const config = {
-	apiKey: 'AIzaSyB5171RmMdJzO131E_Wypm1o1uVsvWBfvw',
-	authDomain: 'leslie-and-snapdocs.firebaseapp.com',
-	databaseURL: 'https://leslie-and-snapdocs.firebaseio.com',
-	projectId: 'leslie-and-snapdocs',
-	storageBucket: '',
-	messagingSenderId: '404074347699',
-};
-const firebaseApp = firebase.initializeApp(config);
+const firebaseApp = firebase.initializeApp(FIREBASE_CONFIG);
 const firebaseDb = firebaseApp.database();
 
 export default firebaseDb;
