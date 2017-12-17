ac40fe048d7f58f6cec6497dee0ce03e70d85084
Leslie Zacharkow <lesliezacharkow@gmail.com>
2017-12-17 12:05:41 -0800

    Employee List component

--- src/components/EmployeesList/EmployeesList.js
+++ src/components/EmployeesList/EmployeesList.js
@@ -1,29 +1,47 @@
 // Presentation component
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import './styles.css';
 
 import ListItem from './ListItem';
 
 function EmployeesList({ employees }) {
 	return (
-		<div>
+		<div className="employee-list__table">
+
+			<div className="employee-list__table__row employee-list__table__row--headings">
+				<div className="employee-list__table__row__heading employee-list__table__row__heading--name">
+					Name
+				</div>
+				<div className="employee-list__table__row__heading employee-list__table__row__heading--status">
+					Status
+				</div>
+				<div className="employee-list__table__row__heading employee-list__table__row__heading--link" />
+			</div>
+
 			{employees.map(employee => (
 				<ListItem
 					key={employee.id}
 					id={employee.id}
-					active={employee.active}
 					name={employee.name}
-					age={employee.age}
-					email={employee.email}
+					active={employee.active}
 				/>
 			))}
+
 		</div>
 	);
 }
 
 EmployeesList.propTypes = {
-	// name: PropTypes.string.isRequired,
+	employees: PropTypes.arrayOf(PropTypes.shape({
+		active: PropTypes.bool,
+		bio: PropTypes.string,
+		date_of_birth: PropTypes.number,
+		email: PropTypes.string,
+		name: PropTypes.string,
+		id: PropTypes.string,
+	})).isRequired,
 };
 
 export default EmployeesList;
--- src/components/EmployeesList/ListItem.js
+++ src/components/EmployeesList/ListItem.js
@@ -1,25 +1,43 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import classnames from 'classnames';
+
 import { EMPLOYEES_ROUTE } from '../../constants';
 
 function ListItem({
 	id,
-	active,
 	name,
-	age,
-	email,
+	active,
 	match,
 }) {
+	const classes = classnames('employee-list__table__row', {
+		'employee-list__table__row--inactive': !active,
+	});
+
+	const activeMessage = active
+		? 'Active'
+		: 'Not active';
+
 	return (
-		<Link to={`${EMPLOYEES_ROUTE}/${id}`}>
-			{id}<br />
-			{active}<br />
-			{name}<br />
-			{age}<br />
-			{email}
-			<hr />
+		<Link className={classes} to={`${EMPLOYEES_ROUTE}/${id}`}>
+			<div className="employee-list__table__row__cell employee-list__table__row__cell--name">
+				{name}
+			</div>
+			<div className="employee-list__table__row__cell employee-list__table__row__cell--status">
+				{activeMessage}
+			</div>
+			<div className="employee-list__table__row__cell employee-list__table__row__cell--link link--primary">
+				View<span> Employee</span>
+			</div>
 		</Link>
 	);
 }
+
+ListItem.propTypes = {
+	id: PropTypes.string.isRequired,
+	name: PropTypes.string.isRequired,
+	active: PropTypes.bool.isRequired,
+};
 
 export default ListItem;
